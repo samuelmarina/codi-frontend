b165717f569004942c9289cef55be309e0835e6d
samuelmarina <samuellmarina@gmail.com>
2021-07-04 14:19:10 -0400

    Crear tests

--- src/components/InputOutputForm/InputOutputForm.test.js
+++ src/components/InputOutputForm/InputOutputForm.test.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "React";
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import React from "React";
+import { render, screen, act } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import Form from "./InputOutputForm";
 import Context from "../../helpers/context/context";
@@ -10,7 +10,7 @@ describe("Component", () => {
 
     beforeEach(() => {
         render(
-            <Context.Provider value={{problemInfo,}}>
+            <Context.Provider value={{problemInfo, setProblemInfo}}>
                 <Form />
             </Context.Provider>
         );
@@ -21,6 +21,24 @@ describe("Component", () => {
         expect(formComponent).toBeInTheDocument();
     });
 
+    it("Should have empty input and output fields", () => {
+        const inputField = document.querySelector("[name=input]");
+        const outputField = document.querySelector("[name=output]");
+        expect(inputField.value).toBe("");
+        expect(outputField.value).toBe("");
+    });
 
+    it("Should have input value '1' and output value '2'", () => {
+        const inputField = document.querySelector("[name=input]");
+        const outputField = document.querySelector("[name=output]");
+        act(() => {
+            userEvent.type(inputField, "1");
+        });
+        act(() => {
+            userEvent.type(outputField, "2");
+        });
+        expect(inputField.value).toBe("1");
+        expect(outputField.value).toBe("2");
+    });
 
 })
